5109639e304af92d684ede8fe56ca66f1284406d
Jody Paul <jody@users.noreply.github.com>
2019-11-06 00:13:14 -0700

    Package renaming (#80)
    • Update and rename src/main/java/QualityToolsForBlueJ/Library.java to src/main/java/edu/msudenver/bluejext/qualitytools/Library.java
    • Update Library.java to use new package name

---
+++ src/main/java/edu/msudenver/bluejext/qualitytools/Library.java
@@ -0,0 +1,10 @@
+/*
+ * Example source code for the Quality Tools for BlueJ project.
+ */
+package edu.msudenver.bluejext.qualitytools;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
